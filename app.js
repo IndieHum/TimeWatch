1ae9094f484c81ade37ee71d7cbe4516aec9c39f
IndieHum <moein.fereyduni@gmail.com>
2024-07-11 19:31:29 +0430

    fixing logic of timewatch

--- app.js
+++ app.js
@@ -9,45 +9,51 @@ let SecondsNum = 0;
 let MainInterval;
 
 function StartBtnHandler() {
-  MainInterval = setInterval(() => {
-    SecondsNum++;
-    Seconds.innerText = SecondsNum >= 10 ? SecondsNum : `0${SecondsNum}`;
-    switch (SecondsNum) {
-      case 12:
-        MinutesNum++;
-        Minutes.innerText = MinutesNum >= 10 ? MinutesNum : `0${MinutesNum}`;
-        SecondsNum = 0;
-        break;
-    }
-    switch (MinutesNum) {
-      case 61:
-        alert("you made that!");
-        return;
-    }
-  }, 1000);
-  StartBtn.style.cursor = "not-allowed";
-  this.disabled = true;
+    MainInterval = setInterval(() => {
+        SecondsNum++;
+        Seconds.innerText = SecondsNum >= 10 ? SecondsNum : `0${SecondsNum}`;
+        switch (SecondsNum) {
+            case 59:
+                Seconds.innerText = "00";
+                MinutesNum++;
+                Minutes.innerText =
+                    MinutesNum >= 10 ? MinutesNum : `0${MinutesNum}`;
+                SecondsNum = 0;
+                break;
+        }
+        switch (MinutesNum) {
+            case 60:
+                alert("you made that!");
+                return;
+        }
+    }, 1000);
+    StartBtn.style.cursor = "not-allowed";
+    StartBtn.disabled = true;
 }
 
 function StopBtnHandler() {
-  StartBtn.disabled = false;
-  StartBtn.style.cursor = "default";
-  clearInterval(MainInterval);
+    StartBtn.disabled = false;
+    StartBtn.style.cursor = "default";
+    clearInterval(MainInterval);
 }
 
 function ResetBtnHandler() {
-  clearInterval(MainInterval);
-  StartBtn.disabled = false;
-  StartBtn.style.cursor = "default";
-  Minutes.innerText = "00";
-  Seconds.innerText = "00";
+    clearInterval(MainInterval);
+    StartBtn.disabled = false;
+    StartBtn.style.cursor = "default";
+    Minutes.innerText = "00";
+    Seconds.innerText = "00";
+    MinutesNum = 0;
+    SecondsNum = 0;
 }
 
 window.addEventListener("blur", () => {
-  document.title = "Time Goes On...";
+    if (StartBtn.disabled) {
+        document.title = "Time Goes On...";
+    } else document.title = DocTitle;
 });
 window.addEventListener("focus", () => {
-  document.title = DocTitle;
+    document.title = DocTitle;
 });
 
 StartBtn.addEventListener("click", StartBtnHandler);
